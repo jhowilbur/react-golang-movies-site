96c75bc9a7523733f29028c67ea6738dc72c302a
wilbur <wilbur@wilbur.com>
2023-04-25 12:06:40 -0400

    - first commit

---
+++ src/components/Movies.js
@@ -0,0 +1,14 @@
+const Movies = () => {
+    return(
+        <>
+
+            <div className="text-center">
+                <h2>Find a movie to watch tonight!</h2>
+                <hr />
+            </div>
+
+        </>
+    )
+}
+
+export default Movies;
