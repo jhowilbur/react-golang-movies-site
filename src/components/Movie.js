682225e1d8ce4180d3713a08f70001c92aa4db13
wilbur <wilbur@wilbur.com>
2023-04-26 11:08:14 -0400

    - update

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,37 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
 const Movie = () => {
+    const [movie, setMovie] = useState({});
+    let {id} = useParams();
+
+    useEffect(() => {
+        let myMovie = {
+            id: 1,
+            title: "The Shawshank Redemption",
+            release_date: "1994-09-14",
+            poster: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+            runtime: 142,
+            mpaa_rating: "R",
+            description: "Long description here"
+        };
+
+        setMovie(myMovie);
+    }, [id]);
+
     return(
         <>
-
             <div className="text-center">
-                <h2>Movie</h2>
+                <h2>{movie.title}</h2>
                 <hr />
+                <small><em>{movie.release_date}</em></small>
+                <br />
+                <small><em>{movie.mpaa_rating}</em></small>
+                <br />
+                <small><em>{movie.runtime} minutes</em></small>
+                <br /><br />
+                <img src={movie.poster} alt={movie.title} width="400" height="500"></img>
             </div>
-
         </>
     )
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,67 @@
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
 const Movies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        let movieList = [
+            {
+                id: 1,
+                title: "The Shawshank Redemption",
+                release_date: "1994-09-14",
+                poster: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+                runtime: 142,
+                mpaa_rating: "R",
+                description: "Long description here"
+            },
+            {
+                id: 2,
+                title: "The Godfather",
+                release_date: "1972-03-14",
+                poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+                runtime: 175,
+                mpaa_rating: "R",
+                description: "Long description here"
+            },
+        ];
+
+        setMovies(movieList)
+
+    }, []);
+
     return(
         <>
 
             <div className="text-center">
                 <h2>Movies</h2>
                 <hr />
+                <table className="table table-striped table-over">
+                    <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+                        <th>Poster</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {movies.map((movie) => (
+                            <tr key={movie.id}>
+                                <td>
+                                    <Link to={`/movies/${movie.id}`}>
+                                        {movie.title}
+                                    </Link>
+                                </td>
+                                <td>{movie.release_date}</td>
+                                <td>{movie.mpaa_rating}</td>
+                                <td>
+                                    <img src={movie.poster} alt={movie.title} width="100" height="150"></img>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
 
         </>
